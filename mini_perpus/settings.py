305d771144d9e76401335528b572ca1c699445d6
kezialasma <kezia.lasma@ui.ac.id>
2023-11-20 15:51:14 +0700

    coba deploy TI

--- mini_perpus/settings.py
+++ mini_perpus/settings.py
@@ -11,12 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import environ
-import os 
+import environ # Tambahkan kode berikut
+import os # Tambahkan kode berikut
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 env = environ.Env()
 
 
